3b54b2908922c35dca6838f74d7245fc1104f887
Emily Miles <emiles4@gmail.com>
2018-09-26 04:52:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase=/^[A-Z]+$/
+  elsif phrase=(/^[A-Z]+$/)
     return "NO, NOT SINCE 1938!"
   else return "HUH?! SPEAK UP SONNY!"
 
