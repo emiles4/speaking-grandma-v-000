1c36de1e77847492d02545eeec0fb35d37cf21a8
Emily Miles <emiles4@gmail.com>
2018-09-26 04:43:37 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-elsif phrase = upcase
+  if phrase = upcase
   puts "NO, NOT SINCE 1938!"
     if phrase = "I LOVE YOU GRANDMA!"
       puts "I LOVE YOU TOO PUMPKIN!"
