af99d1ba151fab68f82dbad39fc43159daea1c24
Emily Miles <emiles4@gmail.com>
2018-09-26 04:44:33 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase = /[A-Z]
+  if phrase = /[A-Z]/
   puts "NO, NOT SINCE 1938!"
     if phrase = "I LOVE YOU GRANDMA!"
       puts "I LOVE YOU TOO PUMPKIN!"
